15d5c30677f70589005210aadf16c111bf13fe17
Eli Yacobov <eli.yacobov1@gmail.com>
2021-06-03 12:14:11 +0300

    created json parsing function for json valued columns

--- regression.py
+++ regression.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import json
 
 ################################################
 #
@@ -7,23 +7,30 @@ import pandas as pd
 #
 ################################################
 
+
+def parse_col_vals(entry):
+    ret_val = ''
+    for obj in json.loads(entry.replace('\'', '\"')):
+        for k, v in obj.items():
+            if k != 'id':
+                ret_val += v + '*'
+    return ret_val[:-1] if ret_val != '' else ret_val
+
+
 def pre_process(path):
     """
     this function pre-processes the data samples in the given path (path should be a csv file)
     """
-    data = pd.read_csv(path)
-    data.join(data.pop('genre').str.join('|').str.get_dummies())
-    # filters = (1 <= data['condition']) & (data['condition'] <= 5)  # condition within valid range
-    # filters = filters & data['date'].notnull() & data['id'].notnull()  # essential fields date and id are present
-    # filters = filters & (data['price'] > 0) & (data['sqft_living'] > 0) \
-    #           & (data['floors'] > 0) & (data['sqft_lot15'] > 0)  # non-negative values on corresponding fields
-    data = data.loc[filters]  # apply conditions
-    data = data.drop_duplicates()  # drop duplicates
-    # separate year and month and convert them into dummy-variables
-    sliced_dates = pd.concat([data['date'].str.slice(4, 6), data['date'].str.slice(0, 4)], axis=1)
-    data = pd.concat([data, pd.get_dummies(sliced_dates.iloc[:, 0]), pd.get_dummies(sliced_dates.iloc[:, 1])], axis=1)
-    data = data.drop(['id'], axis=1)  # drop categorical features
-    return data
+    df = pd.read_csv(path)
+    json_cols = ['genres']
+
+    # col_items = [item for item in df.pop('genres')]
+    for col in json_cols:
+        for i in range(df.shape[0]):
+            df.loc[i, col] = parse_col_vals(df.loc[i, col])
+        df = pd.concat([df, df[col].str.get_dummies(sep='*')], axis=1)
+        df.drop(col, axis=1)
+    return df
 
 
 def fit(path):
@@ -37,5 +44,8 @@ def predict(csv_file):
     :param csv_file: csv with movies details. Same format as the training dataset csv.
     :return: a tuple - (a python list with the movies revenues, a python list with the movies avg_votes)
     """
-
     pass
+
+
+if __name__ == '__main__':
+    pre_process('./movies_dataset.csv')
